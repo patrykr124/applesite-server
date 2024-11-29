a0174993203306ceecab45be3e8e8a481de0da0a
Patryk <patrykr124@gmail.com>
2024-11-29 15:18:26 +0100

    29.10

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const corsOptions = {
     origin: [
         "http://localhost:5173",
-        "https://apple-site-5juj1s6hq-patryks-projects-2d4eefcb.vercel.app",
+        "https://applesite-server.vercel.app/",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: false,
